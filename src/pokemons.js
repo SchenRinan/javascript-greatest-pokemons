581ad4fb8df914fd4669217fab645775ca2c6d1d
Rinan <jandrewschnell@gmail.com>
2023-04-06 22:07:02 +0200

    done day 0

--- src/pokemons.js
+++ src/pokemons.js
@@ -1,7 +1,26 @@
 // Iteration 1: All Fire Pokemons- get the array of all Fire type pokemons
-
+function getAllFirePokemons(array){
+    const cloneArray = [];
+    for(i=0;i<array.length;i++){cloneArray.push(array[i]);};
+    const onlyFire = cloneArray.filter(fireMon => fireMon.type.includes("Fire"));
+    return onlyFire;
+}
 // Iteration 2: Shortest Pokemon- find the `name` of the shortest pokemon
-
+function shortestPokemon(array){
+    const cloneArray = [];
+    for(i=0;i<array.length;i++){cloneArray.push(array[i]);};
+    const removem = [];
+    for(i=0;i<array.length;i++){
+        cloneArray[i].height = cloneArray[i].height.replace(" m","");
+    }
+    let shorttracker = 9999;
+    let pokeNum = 0;
+    for(i=0;i<cloneArray.length;i++){
+        if(shorttracker > +cloneArray[i].height){shorttracker = +cloneArray[i].height; pokeNum = i;}
+    }
+    if(array.length === 0){return 0;}
+    return cloneArray[pokeNum].name;
+}
 // Iteration 3: candy_count average - average of `candy_count` for all the pokemons
 
 // Iteration 4: images for the first 10 `Ground`  Pokemons
