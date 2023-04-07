b82a659166d73d5cd920deb113518617dead8be7
Rinan <jandrewschnell@gmail.com>
2023-04-07 08:03:47 +0200

    day 1 before breakfast

--- src/pokemons.js
+++ src/pokemons.js
@@ -22,9 +22,21 @@ function shortestPokemon(array){
     return cloneArray[pokeNum].name;
 }
 // Iteration 3: candy_count average - average of `candy_count` for all the pokemons
+function candyAverage(array){
+    if(array.length === 0){return 0;}
+    const filterCandy = array.filter(a => typeof a.candy_count === "number");
+    const sumCandy = filterCandy.reduce((a, b) => a + b.candy_count, 0);
+    return Math.round(sumCandy / array.length *100)/100;
+}
 
 // Iteration 4: images for the first 10 `Ground`  Pokemons
-
+function getGroundPokeImg(array){
+    if(array.length === 0){return 0;}
+    const filterGround = array.filter(a => a.type.includes("Ground"));
+    const tenGround = [];
+    for(i=0;i<10 & i < filterGround.length; i++){tenGround.push(filterGround[i].img)}
+    return tenGround;
+}
 // Iteration 5: Find all pokemon names heavier than Pikachu
 
 // Iteration 6: Alphabetic Order - Order by name and print the first 20 names
